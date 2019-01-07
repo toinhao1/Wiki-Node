09c09e66ab7267e3a8ef3e2336d586e75c8168be
toinhao1 <toinhao89@gmail.com>
2019-01-07 18:17:33 -0300

    Fixed some issues

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 const logger = require('morgan');
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
 
 module.exports = {
-  init(){
+  init(app, express){
     app.use(logger('dev'));
-
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true}));
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(expressValidator());
   }
 };
